8915abf3230062d55915efb5f93c23df823b4b40
Mariah Wear <mariahweardev@gmail.com>
2023-11-17 12:31:58 -0700

    fixed sizing on portfolio about me picture

--- src/components/thankyou.jsx
+++ src/components/thankyou.jsx
@@ -3,9 +3,8 @@
 function ThankYou() {
     return (
         <div className="container py-4">
-        <h2 className="h2" id="Thank-You">Contact Me:</h2>
+        <h2 className="h2" id="Thank-You">Successfully Submitted Contact Form</h2>
 
-        <h3>Successfully Submitted Contact Form</h3>
         <p>Thank you! I&apos;ll be in touch soon. </p>
         
        
