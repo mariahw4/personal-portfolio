189a734da70fff8cb5d96a3f31f7ae42e3edcc2c
Mariah Wear <mariahweardev@gmail.com>
2023-11-17 12:29:05 -0700

    fixed language on thanks page

--- src/components/thankyou.jsx
+++ src/components/thankyou.jsx
@@ -4,7 +4,9 @@ function ThankYou() {
     return (
         <div className="container py-4">
         <h2 className="h2" id="Thank-You">Contact Me:</h2>
-        <p>Thank you! I&aposll be in touch soon. </p>
+
+        <h3>Successfully Submitted Contact Form</h3>
+        <p>Thank you! I&apos;ll be in touch soon. </p>
         
        
 
