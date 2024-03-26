cd810eebd62b67b8febcbc9172e1b4d81e395c0e
Mariah Wear <mariahweardev@gmail.com>
2024-03-25 20:55:23 -0600

    fixed sizing for nav bar

--- src/components/NavLinks.jsx
+++ src/components/NavLinks.jsx
@@ -3,7 +3,7 @@ import PropTypes from "prop-types";
 export default function NavLinks({ links }) {
   return (
     <>
-      <nav className="navbar navbar-expand-lg bg-secondary">
+      <nav className="navbar navbar-dark navbar-expand-lg bg-secondary">
         <div className="container-fluid">
           <div className="" id="navbarSupportedContent">
             <ul className="navbar-nav me-auto mb-2 mb-lg-0">
