9c4263393325e4d781a8e8428410b8ef439c6251
Mariah Wear <mariahweardev@gmail.com>
2024-03-25 21:49:59 -0600

    updated headshot

--- src/components/home.jsx
+++ src/components/home.jsx
@@ -5,7 +5,7 @@ function Home() {
         <div className="container py-4">
             <h2 className="h2" id="Home">Hello! I&apos;m Mariah - Nice to meet you!! </h2>
             <div className="justify-content-center">
-                <img src="/images/headshot.png" className="headshot-img" alt=" headshot "/>
+                <img src="/images/new-headshot.png" className="headshot-img" alt=" headshot "/>
 
             </div>
         </div>
